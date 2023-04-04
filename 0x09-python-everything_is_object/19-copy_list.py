775551cf542b1aad9463e6ba016dd2772fcb0358
Owolabi Pius <opius2007@gmail.com>
2023-04-04 18:40:06 +0100

    Python Everything is Object Task 19 update

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-
 def copy_list(clst):
-    """
-    This function takes a list as input and returns a copy of the input list.
-    If the input is not a list, it returns the input as it is.
-    """
     return clst.copy() if isinstance(clst, list) else clst
