b59b126bd2e476f50816905e8db92390ce18bef3
Owolabi Pius <opius2007@gmail.com>
2023-04-11 17:36:12 +0100

    Python Inheritance Tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,12 +3,11 @@
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
-    
+
     Args:
         obj (object): An object to look up.
-        
+
     Returns:
         list: A list of available attributes and methods of the object.
     """
     return dir(obj)
-
