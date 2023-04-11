d1f793b8d8d9fae0809e671d342d3073dc1be970
Owolabi Pius <opius2007@gmail.com>
2023-04-11 19:16:53 +0100

    Python Inheritance Tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Class representing a geometry shape.
+    """
+
+    def area(self):
+        """
+        Method to calculate the area of the geometry shape.
+
+        Raises:
+            Exception: Always, as this method is not implemented.
+        """
+        raise Exception("area() is not implemented")
