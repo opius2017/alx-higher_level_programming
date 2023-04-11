e322be1364962e304e3635999f253b8e5cf62bcb
Owolabi Pius <opius2007@gmail.com>
2023-04-11 18:19:18 +0100

    Python Inheritance Tasks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 class MyList(list):
-    """
-    MyList is a subclass of the built-in list class. It inherits all the
-    attributes and methods of list, and adds a new public instance method
-    print_sorted() that prints the list in ascending order.
-    """
+    """ MyList is a subclass of the built-in list class. """
 
     def print_sorted(self):
         """ Prints the list in ascending order. """
