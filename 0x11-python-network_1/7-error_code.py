7e7118b196cfebbbdf227d3fa85c542041a59d9e
Owolabi Pius <opius2007@gmail.com>
2023-06-29 13:55:48 +0100

    Update Python Network task file

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
+"""Script that sends a request to a given URL and displays the response body.
 Usage: ./7-error_code.py <URL>
   - Handles HTTP errors.
 """
