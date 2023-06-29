233bdb072186fddfa5a67bb06cfd128dcc98908e
Owolabi Pius <opius2007@gmail.com>
2023-06-29 13:57:07 +0100

    Update Python Network task file

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+"""Script that sends a POST request to a given URL with a given email.
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response.
 """
