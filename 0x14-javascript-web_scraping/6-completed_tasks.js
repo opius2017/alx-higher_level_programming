a8385db2ab6e18d5eeebe8de3ae8568d92bdc877
Owolabi Pius <opius2007@gmail.com>
2023-07-25 16:18:38 +0100

    update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
-const request = require('request');
+const axios = require('axios');
+
 const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
+axios.get(url)
+  .then(response => {
     const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
+    const tasks = response.data;
+    for (const task of tasks) {
       if (task.completed === true) {
         if (completed[task.userId] === undefined) {
           completed[task.userId] = 1;
@@ -20,7 +18,7 @@ request(url, function (err, response, body) {
       }
     }
     console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
-});
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
